e2986bf2f43d29aac7f01334ec25ee401aafdfa3
Alejandro Romero <alejo8591@gmail.com>
2012-06-13 16:25:19 -0500

    update and push app

--- alejo8591/main.py
+++ alejo8591/main.py
@@ -4,7 +4,7 @@
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# You may obtain ax copy of the License at
 #
 #     http://www.apache.org/licenses/LICENSE-2.0
 #
@@ -15,10 +15,24 @@
 # limitations under the License.
 #
 import webapp2
+form = """
+           <form method="post" action="/testform">
+             <input name="q">
+             <input type="submit">
+           </form>
+       """
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.out.write('Hello Udacity!')
+        self.response.out.write(form)
+        
+class TestHandler(webapp2.RequestHandler):
+    def post(self):
+        #q = self.request.get("q")
+        #self.response.out.write(q)
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.out.write(self.request)
 
-app = webapp2.WSGIApplication([('/', MainHandler)],
+app = webapp2.WSGIApplication([('/', MainHandler),
+                               ('/testform', TestHandler)],
                               debug=True)
