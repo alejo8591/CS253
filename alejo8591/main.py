41f758893dddb7d346a179831de7cc5eade67788
Alejandro Romero <alejo8591@gmail.com>
2012-06-15 00:13:52 -0500

    update main for the app

--- alejo8591/main.py
+++ alejo8591/main.py
@@ -20,25 +20,71 @@ form = """
              What is your birthday?
              <br>
              <label> Month
-                <input type="text" name="month">
+                <input type="text" name="month" value="%(month)s">
              </label>
              <label> Day
-                <input type="text" name="day">
+                <input type="text" name="day" value="%(day)s">
             </label>
              <label> Year
-                <input type="text" name="year">
+                <input type="text" name="year" value="%(year)s">
             </label>
              <br>
+             <div style="color : red">%(error)s</div>
              <input type="submit">
            </form>
        """
 
 class MainHandler(webapp2.RequestHandler):
+    def write_form(self, error="", month="", day="", year=""):
+        self.response.out.write(form % {"error":error,
+                                        "month": month,
+                                        "day": day,
+                                        "year":year})
+        
+    def valid_month(self, month):
+        months = ['January',
+              'February',
+              'March',
+              'April',
+              'May',
+              'June',
+              'July',
+              'August',
+              'September',
+              'October',
+              'November',
+              'December']
+        month = month.capitalize()
+        if(month in months): return month
+     
+    def valid_day(self, day):
+        days = range(1,32)
+        day = int(day)
+        if(day in days): return day
+    
+    def valid_year(self, year):
+        years = range(1900, 2021)
+        year = int(year)
+        if(year in years): return year
+        
     def get(self):
-        self.response.out.write(form)
+           self.write_form()
         
     def post(self):
-        self.response.out.write("Thanks")
+        
+        user_month = self.request.get('month')
+        user_day = self.request.get('day')
+        user_year = self.request.get('year')
+        
+        month = self.valid_month(self.request.get('month'))
+        day = self.valid_day(self.request.get('day'))
+        year = self.valid_year(self.request.get('year'))
+        
+        if not (month and day and year):
+            self.write_form("That dosen't look valid to me, friend.",
+                            user_month, user_day, user_year)
+        else:
+            self.response.out.write('Thanks!')
         
 
 app = webapp2.WSGIApplication([('/', MainHandler)],
